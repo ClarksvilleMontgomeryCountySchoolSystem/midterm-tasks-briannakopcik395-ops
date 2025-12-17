2cf8bf13a703a40291859c6b36a8d09b986f68dd
briannakopcik395-ops <brianna.kopcik395@students.cmcss.net>
2025-12-17 11:09:59 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,31 @@
+#Given variables
+party_pizza_mini = 14
+large = 8
+medium = 6
+people = 6 # friends
 
+#Do Not use crtl+A to select code.  Only copy the code below this line.
+#------------------------------------------------------------------------------------------------
+total number of slices: 42 + 1
+print(f"Total number of slices + people")
+
+people + 1
+share = slices
+Leftover = 0
+share= 3
+print(f"shared slices + leftovers")
+
+people = +2 #Eric and Brandon are coming too.
+share = /2
+left over = +2
+print(f"people + pizza)")
+print(f"pizza + people / 2")
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+slices = +5
+share = /2
+left over = 14
+print(f" 5 14 / 2")
+print(f"pizza slices left over")
+print("...for Mr. Hollow Leg")
