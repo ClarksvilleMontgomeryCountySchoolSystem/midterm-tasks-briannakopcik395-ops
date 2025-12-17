df83ed62a2b81e6d772d5a10da0493ef6353d35c
briannakopcik395-ops <brianna.kopcik395@students.cmcss.net>
2025-12-17 11:11:06 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,29 @@
+# Testing flag - will be set by test
+TESTING = True  # <-- Should be False by default
+item = None
+price = None
+quantity = None
 
+print("""
+========================================
+   WELCOME TO THE PECULIAR EMPORIUM!
+   "Magical items at mundane prices!"
+   Prosperity comes in threes!
+========================================
+ITEM MENU:
+Invisibility Cloak.........$44.99
+Dragon Egg.....................$29.99
+""")
+
+menu ='''
+Flying Carpet...............$119.99
+phoenix Feather.........$14.99
+Time Turner......$84.99
+Enchatnted Sword.....$65.99
+Potion of Luck......$11.99
+Crystal Ball.....39.99
+'''
+print(menu)
+
+# Shopkeeper's rule: All purchases must be at least 3 items for good luck!
+# (Don't worry - the shopkeeper checks every orde
